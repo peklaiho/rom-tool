76e4b811dc59e033e22158e8b61aaf0b3e04c008
Pekka Laiho <pekka.i.laiho@gmail.com>
2025-06-29 11:32:08 +0300

    Applying IPS patches to SNES ROMs seems to be working

--- main.go
+++ main.go
@@ -9,6 +9,38 @@ import (
 	"os"
 )
 
+// Buffer that wraps a byte array
+type Buffer struct {
+	data []byte
+}
+
+// Expand buffer if needed
+func (b *Buffer) Expand(length int) {
+	if len(b.data) < length {
+		newBuf := make([]byte, length)
+		copy(newBuf, b.data)
+		b.data = newBuf
+	}
+}
+
+func (b *Buffer) WriteByte(offset int, value byte, length int) {
+	b.Expand(offset + length)
+	for i := 0; i < length; i++ {
+		b.data[offset + i] = value
+	}
+}
+
+func (b *Buffer) WriteBytes(offset int, value []byte) {
+	b.Expand(offset + len(value))
+	for i := 0; i < len(value); i++ {
+		b.data[offset + i] = value[i]
+	}
+}
+
+func (b *Buffer) GetData() []byte {
+	return b.data
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage:")
@@ -31,16 +63,47 @@ func main() {
 	}
 }
 
-func applyPatch(buffer bytes.Buffer, patch bytes.Reader) {
+func readData(patch *bytes.Reader, length int) []byte {
+	data := make([]byte, length)
+	_, err := io.ReadFull(patch, data)
+	if err == io.EOF {
+		return make([]byte, 0)
+	} else if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func applyPatch(buffer *Buffer, patch *bytes.Reader) {
 	// Read and verify header
-	header := make([]byte, 5)
-	_, err := io.ReadFull(patch, header)
-	if err != nil || string(header) != "PATCH" {
+	header := readData(patch, 5)
+	if len(header) == 0 || string(header) != "PATCH" {
 		panic("Invalid IPS file")
 	}
 
 	for {
+		// Read patch record
+		record := readData(patch, 3)
+		if len(record) == 0 || string(record) == "EOF" {
+			break
+		}
 
+		// Read offset
+		offset := int(binary.BigEndian.Uint32(append([]byte{0}, record...)))
+
+		// Read size
+		size := int(binary.BigEndian.Uint16(readData(patch, 2)))
+
+		if size == 0 {
+			// RLE encoded
+			rleSize := int(binary.BigEndian.Uint16(readData(patch, 2)))
+			rleValue := readData(patch, 1)
+			buffer.WriteByte(offset, rleValue[0], rleSize)
+		} else {
+			// Normal patch
+			data := readData(patch, size)
+			buffer.WriteBytes(offset, data)
+		}
 	}
 }
 
@@ -52,7 +115,7 @@ func cmdIps(romFile string, patchFiles []string) {
 
 	fmt.Fprintf(os.Stderr, "Patching ROM: %s (%x)\n", romFile, sha1.Sum(rom))
 
-	buffer := bytes.NewBuffer(rom)
+	buffer := Buffer{rom}
 
 	for _, patchFile := range patchFiles {
 		ips, err := os.ReadFile(patchFile)
@@ -62,10 +125,14 @@ func cmdIps(romFile string, patchFiles []string) {
 
 		fmt.Fprintf(os.Stderr, "Applying patch: %s (%x)\n", patchFile, sha1.Sum(ips))
 
-		applyPatch(buffer, bytes.NewReader(ips))
+		applyPatch(&buffer, bytes.NewReader(ips))
 	}
 
 	// Write file
+	result := buffer.GetData()
+	resultFile := romFile + "-patched"
+	fmt.Fprintf(os.Stderr, "Writing result file: %s (%x)\n", resultFile, sha1.Sum(result))
+	os.WriteFile(resultFile, result, 0644)
 }
 
 func cmdSha1(filename string) {
